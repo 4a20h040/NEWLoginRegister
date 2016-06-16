c015d4b67c7501f476d8125783515e115782e142
4a20h040 <lijiahuei01@gmail.com>
2016-06-16 14:03:14 +0800

    Update Register.php

--- Register.php
+++ Register.php
@@ -1,13 +1,13 @@
 <?php
-    $con = mysqli_connect("mysql10.000webhost.com", "a3288368_user", "abcd1234", "a3288368_data");
+    $con = mysqli_connect("mysql6.000webhost.com", "a8226888_user", "4a20h040", "a8226888_data");
     
     $name = $_POST["name"];
-    $age = $_POST["age"];
+    $email = $_POST["email"];
     $username = $_POST["username"];
     $password = $_POST["password"];
 
-    $statement = mysqli_prepare($con, "INSERT INTO user (name, username, age, password) VALUES (?, ?, ?, ?)");
-    mysqli_stmt_bind_param($statement, "siss", $name, $username, $age, $password);
+    $statement = mysqli_prepare($con, "INSERT INTO user (name, username, email, password) VALUES (?, ?, ?, ?)");
+    mysqli_stmt_bind_param($statement, "siss", $name, $username, $email, $password);
     mysqli_stmt_execute($statement);
     
     $response = array();
